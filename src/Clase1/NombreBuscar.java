6e1eff1b5519a9e940e1d9052d7781b868e0edf2
Alejandra <mariaalejandragonzalez2414@gmail.com>
2024-08-16 14:32:50 -0500

    commit1

---
+++ src/Clase1/NombreBuscar.java
@@ -0,0 +1,20 @@
+public class agregarElemento {
+
+    private String nombre;
+
+
+    //metodo constructor - es el único que permite inicializar atributos más de una vez
+    public NombreBuscar(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+}
